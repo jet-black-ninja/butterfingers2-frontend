110983ee1c8d905d94de9debfcf4df36c633024e
jet_black_ninja <pramodkmr958@gmail.com>
2024-11-07 15:56:33 +0530

    more UI components

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -7,3 +7,5 @@ export { default as TextButton } from './TextButton/TextButton';
 export { default as InputField } from './InputField/InputField';
 export { default as Tooltip } from './Tooltip/Tooltip';
 export { default as Switch } from './Switch/Switch';
+export { default as Modal } from './Modal/Modal';
+export { default as NameAndValue } from './NameAndValue/NameAndValue';
