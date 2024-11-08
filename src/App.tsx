7969586a8df382f0ec1d5821f790236dce6a86dc
jet_black_ninja <pramodkmr958@gmail.com>
2024-11-08 10:50:35 +0530

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import './App.module.scss';
 import Header from './components/Header/Header';
 import Typing from './components/Typing/Typing';
 import Footer from './components/Footer/Footer';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { useWindowDimensions } from './hooks';
+import { TypingContext } from './contexts/typing.context';
 
 function App() {
+  const { typingFocused, resultPreview, typemodeVisible, onPreviewResult } =
+    useContext(TypingContext);
   const [roomCode, setRoomCode] = useState<string | null>(null);
+  const [windowWidth] = useWindowDimensions();
   return (
     <>
-      <Header roomCode={roomCode} />
+      <Header
+        roomCode={roomCode}
+        windowWidth={windowWidth}
+        onLogoClick={() => onPreviewResult(null)}
+        onLeaveRoom={() => setRoomCode(null)}
+      />
       <main>
         <Typing />
       </main>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,71 @@
-function Header() {
-  return <header>Header</header>;
+import { ProfileContext } from '@/contexts/profile.context';
+import { TypingContext } from '@/contexts/typing.context';
+import { useContext } from 'react';
+import styles from './Header.module.scss';
+import { ButtonRounded, Loading, Logo } from '../UI';
+import { Icon1v1, IconAccount, IconCustomize, IconLeave } from '@/assets/image';
+interface Props {
+  windowWidth: number;
+  roomCode: string | null;
+  onLogoClick: () => void;
+  onLeaveRoom: () => void;
+}
+function Header(props: Props) {
+  const { windowWidth, roomCode, onLogoClick, onLeaveRoom } = props;
+  // const { activeModal, onOpenModal } = useContext(ModalContext);
+  const { typingFocused } = useContext(TypingContext);
+  const { profile, loadingUser } = useContext(ProfileContext);
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerLogoButtonWrapper}>
+        <div onClick={() => onLogoClick()}>
+          <Logo colored={!typingFocused} />
+        </div>
+        <div
+          className={`opacity-transition ${typingFocused ? 'hide' : ''} ${styles.headerButtons}`}
+        >
+          <ButtonRounded className={styles.headerBtn}>
+            <IconCustomize />
+            {windowWidth > 600 && <span>Customize</span>}
+          </ButtonRounded>
+          {!roomCode ? (
+            <ButtonRounded className={styles.headerBtn}>
+              <Icon1v1 />
+              {windowWidth > 510 && <span>1v1 (Multiplayer)</span>}
+            </ButtonRounded>
+          ) : (
+            <ButtonRounded className={styles.headerBtn} onClick={onLeaveRoom}>
+              <IconLeave />
+              {windowWidth > 510 && <span>Leave Room</span>}
+            </ButtonRounded>
+          )}
+          {profile.username ? (
+            <ButtonRounded
+              className={`${styles.headerBtn} ${styles.accountBtn}`}
+            >
+              <IconAccount className={styles.accountBtnIcon} />
+              <span className={styles.accountBtnText}>{profile.username}</span>
+            </ButtonRounded>
+          ) : (
+            <ButtonRounded
+              className={`${styles.headerBtn} ${styles.accountBtn}`}
+            >
+              <IconAccount className={styles.accountBtnIcon} />
+              {windowWidth > 575 &&
+                (loadingUser ? (
+                  <Loading
+                    type="spinner"
+                    className={styles.loadingUserSpinner}
+                  />
+                ) : (
+                  <span>Account</span>
+                ))}
+            </ButtonRounded>
+          )}
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
