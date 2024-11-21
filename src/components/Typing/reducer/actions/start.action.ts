6e6f612d25ad93b32ab3b969192a448d90bc4199
jet_black_ninja <pramodkmr958@gmail.com>
2024-11-21 10:54:21 +0530

    start action

---
+++ src/components/Typing/reducer/actions/start.action.ts
@@ -0,0 +1,11 @@
+import { TypingState } from "../typing.reducer";
+export default function start(state: TypingState, testType: string): TypingState{
+    return {
+        ...state,
+        dateTypingStarted: state.dateTypingStarted || new Date().getTime(),
+        result: {
+            ...state.result,
+            testType,
+        },
+    }
+}
