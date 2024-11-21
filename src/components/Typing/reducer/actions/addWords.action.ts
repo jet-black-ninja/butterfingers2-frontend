a88a4a64f501a1039d27f1f29fc275367f924a00
jet_black_ninja <pramodkmr958@gmail.com>
2024-11-21 12:49:32 +0530

    restart and add word action

---
+++ src/components/Typing/reducer/actions/addWords.action.ts
@@ -0,0 +1,14 @@
+import { TypingWords } from '../../types';
+import { TypingState } from '../typing.reducer';
+
+export default function addWords(
+  state: TypingState,
+  words: TypingWords
+): TypingState {
+  if (state.result.showResult) return state;
+
+  return {
+    ...state,
+    words: [...state.words, ...words],
+  };
+}
---
+++ src/components/Typing/reducer/actions/restart.action.ts
@@ -0,0 +1,32 @@
+import { TypingWords } from '../../types';
+import { TypingState } from '../typing.reducer';
+
+export default function restart(
+  state: TypingState,
+  words: TypingWords
+): TypingState {
+  return {
+    ...state,
+    words:
+      words ||
+      state.words.map(word => ({
+        isIncorrect: false,
+        chars: word.chars.flatMap(char =>
+          char.type === 'extra' ? [] : { content: char.content, type: 'none' }
+        ),
+      })),
+    wordIndex: 0,
+    charIndex: 0,
+    typed: 0,
+    typedCorrectly: 0,
+    mistype: 0,
+    result: {
+      showResult: false,
+      timeline: [],
+      errors: 0,
+      quoteAuthor: undefined,
+      testType: null,
+    },
+    dateTypingStarted: null,
+  };
+}
